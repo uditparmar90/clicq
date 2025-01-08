754606c23fe4cbf5a82d146603fad310fb377ffb
udit parmar <udittparmar@gmail.com>
2025-01-08 17:26:11 +0530

    data is filtered sucessfull

--- clicq_backend/djangobackend/api/urls.py
+++ clicq_backend/djangobackend/api/urls.py
@@ -1,10 +1,18 @@
 from django.urls import path
-from api import views
-from .views import BrandRetriveUpdateDestroyAPIView,Product_Graphical_UI,ProductsRetriveUpdateDestroyAPIView,BrandListCreateAPIView
+from .views import (
+    RegUsersList,
+    ProductsRetrieveUpdateDestroyAPIView,
+    ProductGraphicalUI,
+    BrandListCreateAPIView,
+    BrandRetrieveUpdateDestroyAPIView,
+    CategoryCreateAPIView,
+)
 
 urlpatterns = [
-    path('regUser/', views.RegUsersList.as_view()),
-	path('Product/', Product_Graphical_UI.as_view(), name='product-list-create'),
-	path('Product/<int:pk>', ProductsRetriveUpdateDestroyAPIView.as_view(), name='product-list-create'),                   
-	path('brands/', BrandListCreateAPIView.as_view(), name='brand-detail'),
+    path('users/', RegUsersList.as_view(), name='user-list'),
+    path('products/<int:brandId>/', ProductsRetrieveUpdateDestroyAPIView.as_view(), name='product-by-brand'),
+    path('products/', ProductGraphicalUI.as_view(), name='product-list'),
+    path('brands/', BrandListCreateAPIView.as_view(), name='brand-list'),
+    path('brands/<int:pk>/', BrandRetrieveUpdateDestroyAPIView.as_view(), name='brand-detail'),
+    path('categories/', CategoryCreateAPIView.as_view(), name='category-list'),
 ]
--- clicq_backend/djangobackend/api/views.py
+++ clicq_backend/djangobackend/api/views.py
@@ -4,47 +4,50 @@ from rest_framework import generics
 
 from .models import UserDetail,Product,Brand,Category
 from .serializers import UserDetailsSerializers,BrandSerializers,CategorySerializers,ProductSerializers
+from rest_framework.response import Response
+from rest_framework import status
+
 
 
 class RegUsersList(ListAPIView):
     queryset=UserDetail.objects.all()
     serializer_class=UserDetailsSerializers
 
-class ProductsRetriveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
-    queryset=Product.objects.all()
-    # print(queryset)
-    serializer_class=ProductSerializers
-    def get_Obj(self):
-        brandId=self.kwargs['pk']
-        print(self.kwargs)
-        if(brandId):
-            resp=Product.objects.filter(brand=brandId)
-            print(resp)
-            return Product.objects.filter(brand=brandId)
-        return super.get_queryset()
-    # def get(self, request, *args, **kwargs):
-    #     try:
-    #         # Fetch filtered queryset
-    #         products = self.get_queryset()
-    #         serializer = self.get_serializer(products, many=True)
-    #         return Response(serializer.data, status=status.HTTP_200_OK)
-    #     except Brand.DoesNotExist:
-    #         return Response({"error": "Brand not found"}, status=status.HTTP_404_NOT_FOUND)
+class ProductsRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializers
+
+    def get_queryset(self):
+        brand_id = self.kwargs.get('brandId')  # Fetch 'brandId' from kwargs
+        if brand_id:
+            return Product.objects.filter(brand_id=brand_id)  # Filter by brand_id
+        return super().get_queryset()
+
+    def get(self, request, *args, **kwargs):
+        try:
+            # Fetch filtered queryset
+            products = self.get_queryset()
+            serializer = self.get_serializer(products, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except Brand.DoesNotExist:
+            return Response({"error": "Brand not found"}, status=status.HTTP_404_NOT_FOUND)
     
 
-class Product_Graphical_UI(ListAPIView):
+class ProductGraphicalUI(ListAPIView):
     queryset=Product.objects.all()
-
     serializer_class=ProductSerializers
 
 
 class BrandListCreateAPIView(ListAPIView):
     queryset=Brand.objects.all()
     serializer_class=BrandSerializers
-class BrandRetriveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
-    queryset=Brand.objects.all()
-    serializer_class=BrandSerializers
+class BrandRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
+    queryset = Brand.objects.all()
+    serializer_class = BrandSerializers
+class BrandRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
+    queryset = Brand.objects.all()
+    serializer_class = BrandSerializers
 
-class CategoryCreateAPLIView(ListAPIView):                              
+class CategoryCreateAPIView(ListAPIView):                              
     queryset=Category.objects.all()
     serializer_class=CategorySerializers
